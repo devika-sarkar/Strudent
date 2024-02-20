b60d0a8e087c4fd38e92ef691c7280227b55be44
devika-sarkar <160656517+devika-sarkar@users.noreply.github.com>
2024-02-21 02:52:29 +0530

    Update first.java

--- first.java
+++ first.java
@@ -12,4 +12,26 @@
       n2=n3;    
      }    
       
-    }}  
+    }}
+class Armstrong {
+public static void main(String[] args) {
+
+	int num,digit;
+	double cubeSum=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number");
+	int n=sc.nextInt();
+	num=n;
+	while(n!=0) {
+		digit=n%10;
+		cubeSum=cubeSum+Math.pow(digit, 3);
+		n/=10;
+	}
+	if(cubeSum==num) {
+		System.out.println("Armstrong: "+ num);
+	}
+	else {
+		System.out.println("not a armstrong");
+	}
+}
+}
